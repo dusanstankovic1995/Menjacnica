e3907420a22bd2a58eaaa4660caafc7db35ee85d
dusanstankovic1995 <dukki95@gmail.com>
2016-03-27 22:39:00 +0200

    Implementirane metode u okviru klase Menjacnica, grana implementacijainterfejsa.

--- Menjacnica/src/Menjacnica.java
+++ Menjacnica/src/Menjacnica.java
@@ -1,26 +1,64 @@
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
+import menjacnica.Valuta;
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	LinkedList<Valuta> kursevi = new LinkedList<Valuta>();
+
 	@Override
 	public void dodavanjeKursa(GregorianCalendar datum, String naziv, String skraceniNaziv, double prodajniKurs,
 			double kupovniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
+		Valuta v = new Valuta();
+		v.setDatum(datum);
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		v.setProdajniKurs(prodajniKurs);
+		v.setSrednjiKurs(srednjiKurs);
+		v.setKupovniKurs(kupovniKurs);
+
+		kursevi.add(v);
 
 	}
 
 	@Override
 	public void brisanjeKursa(GregorianCalendar datum, String naziv, String skraceniNaziv) {
-		// TODO Auto-generated method stub
+		Valuta v = new Valuta();
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		v.setDatum(datum);
+
+		for (int i = 0; i < kursevi.size(); i++) {
+			if (kursevi.get(i).equals(v)) {
+				kursevi.remove(i);
+			}
+			break;
+		}
 
 	}
 
 	@Override
 	public double[] pronadjiIVratiKurs(GregorianCalendar datum, String naziv, String skraceniNaziv) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
+		double[] kurs = new double[3];
+
+		Valuta v = new Valuta();
+		v.setNaziv(naziv);
+		v.setSkraceniNaziv(skraceniNaziv);
+		v.setDatum(datum);
+
+		for (int i = 0; i < kursevi.size(); i++) {
+			if (kursevi.get(i).equals(v)) {
+				kurs[0] = kursevi.get(i).getProdajniKurs();
+				kurs[1] = kursevi.get(i).getSrednjiKurs();
+				kurs[2] = kursevi.get(i).getKupovniKurs();
+			}
+			break;
+
+		}
+		return kurs;
+
+	}
 }
